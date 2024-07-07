58ea72d2691a12ae83e27747cf57e3f7749e04a6
prakash.prasad <infymee@gmail.com>
2024-07-08 05:02:33 +0545

    Major UI changes

--- biomedicalwebsite/settings.py
+++ biomedicalwebsite/settings.py
@@ -160,7 +160,7 @@ USE_TZ = True
 # for vercel 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_URL = "static/"
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 
