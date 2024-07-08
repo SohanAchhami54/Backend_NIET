9df26be7dd61e3537359ead8c66b363d350e5a00
prakash.prasad <infymee@gmail.com>
2024-07-08 08:46:56 +0545

    UI change

--- static/dashboard/js/controller.js
+++ static/dashboard/js/controller.js
@@ -1,6 +1,6 @@
 app.controller("bioController", function ($scope, $http) {
   $scope.postMessage = function (data) {
-    $http.post("http://127.0.0.1:8000/message/", data).then(
+    $http.post("/message/", data).then(
       function (response) {
         console.log("Message sent successfully:", response.data);
         // Show Bootstrap toast message
