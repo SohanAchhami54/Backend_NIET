d2aac85a660d55e131d631818c8dfba9394433e0
prakash.prasad <infymee@gmail.com>
2024-07-09 09:02:40 +0545

    caraousel bug fixed

--- biomedicalwebsite/settings.py
+++ biomedicalwebsite/settings.py
@@ -158,9 +158,9 @@ USE_TZ = True
 
 
 # for vercel 
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 
