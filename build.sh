85656fde4a7bb7f29ff9e17afa5bc9510acba7e1
b0tish <maharjan.saugat1@gmail.com>
2026-03-27 23:57:44 +0545

    changes

--- build.sh
+++ build.sh
@@ -10,3 +10,14 @@ python manage.py collectstatic --no-input
 
 # Run migrations
 python manage.py migrate
+
+# Create a Superuser automatically
+# Replace 'admin', 'admin@example.com', and 'yourpassword' with your desired credentials
+python manage.py shell <<EOF
+from userprofile.models import AppUser
+if not AppUser.objects.filter(email='admin@example.com').exists():
+    AppUser.objects.create_superuser('admin@example.com', 'yourpassword')
+    print("Superuser created.")
+else:
+    print("Superuser already exists.")
+EOF
