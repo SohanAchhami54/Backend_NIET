4379eb36e9500d784d5b24141d0daba293c6f181
b0tish <maharjan.saugat1@gmail.com>
2026-03-28 09:57:23 +0545

    changes

--- biomedicalwebsite/settings.py
+++ biomedicalwebsite/settings.py
@@ -166,7 +166,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.StaticFilesStorage",
     },
 }
 
